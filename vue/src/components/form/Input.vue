34c3e8a641238a18008b48c0e08e41c1a0346c6a
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-09-19 21:36:58 +0300

    #1 - создается форма Input

---
+++ vue/src/components/form/Input.vue
@@ -0,0 +1,15 @@
+<template>
+    <el-input placeholder="Please input" v-model="input"></el-input>
+</template>
+
+<script>
+export default {
+    data: {
+        form: {
+            name: '',
+            date: '',
+            previewUrl: ''
+        }
+    }
+}
+</script>
