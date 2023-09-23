93e2001bf6c247160aa04b00f315ba30039f247b
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-09-23 09:54:47 +0300

    #1 - добавляются отступы

--- vue/src/components/forms/CinemaForm.vue
+++ vue/src/components/forms/CinemaForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <el-input v-model="form.name" placeholder="Please input name" />
-    <el-input v-model="form.year" placeholder="Please input year" />
-    <el-input v-model="form.previewUrl" placeholder="Please input preview URL" />
+    <el-input class="input_margin" v-model="form.name" placeholder="Please input name" />
+    <el-input class="input_margin" v-model="form.year" placeholder="Please input year" />
+    <el-input class="input_margin" v-model="form.previewUrl" placeholder="Please input preview URL" />
   </div>
 </template>
 
@@ -20,3 +20,9 @@ export default {
   }
 }
 </script>
+
+<style lang="less">
+.input_margin {
+  margin-top: 16px;
+}
+</style>
