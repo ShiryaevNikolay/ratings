2781dac466ac1562b4051038528f18861110d072
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-09-23 09:32:43 +0300

    #1 - правки стиля

---
+++ vue/src/components/forms/CinemaForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-input v-model="form.name" placeholder="Please input name" />
+    <el-input v-model="form.year" placeholder="Please input year" />
+    <el-input v-model="form.previewUrl" placeholder="Please input preview URL" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormInput',
+  data() {
+    return {
+      form: {
+        name: '',
+        year: '',
+        previewUrl: '',
+      }
+    }
+  }
+}
+</script>
--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -3,7 +3,7 @@
     <section class="p-16">
       Homepage
       <button @click="() => openHelpModal()">Open modal</button>
-      <FormInput />
+      <CinemaForm />
     </section>
   </PageLayout>
 </template>
@@ -11,14 +11,14 @@
 <script>
 import PageLayout from '../parts/PageLayout'
 import { helpModal } from "@/mixins/modals";
-import FormInput from "../form/FormInput"
+import CinemaForm from "../forms/CinemaForm"
 
 export default {
   name: 'HomePage',
   mixins: [helpModal],
   components: {
     PageLayout,
-    FormInput
+    CinemaForm
   }
 }
 </script>
