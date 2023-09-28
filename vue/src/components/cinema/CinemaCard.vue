9e690fc5a964d9100529104899843fd7822dcc4c
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-09-28 21:42:12 +0300

    #4 - карточка с отображением фильма

---
+++ vue/src/components/cinema/CinemaCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="cinema-card">
+    <el-row>
+      <img class="cinema-card__image" :src="getPreviewUrl()"/>
+    </el-row>
+    <el-row>
+      <span class="cinema-card__element cinema-card__element_title">{{ cinema.name }}</span>
+    </el-row>
+    <el-row>
+      <span class="cinema-card__element">{{ cinema.year }}</span>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CinemaCard',
+  props: {
+    cinema: Object,
+    default: () => (null)
+  },
+  computed: {
+    getPreviewUrl () {
+      return this.props.cinema.previewUrl
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.cinema-card {
+  padding-bottom: 16px;
+  border: solid #DCDCDC;
+  border-radius: 10px;
+
+  &__image {
+    max-width: 100%;
+    border-radius: 8px 8px 0px 0px;
+  }
+
+  &__element {
+    display: block;
+    font-family: 'Courier New', Courier;
+    margin-top: 8px;
+    padding-left: 16px;
+    padding-right: 16px;
+
+    &_title {
+      font-size: 18px;
+      font-weight: 500;
+    }
+  }
+}
+</style>
--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -4,6 +4,9 @@
       Homepage
       <button @click="() => openHelpModal()">Open modal</button>
       <CinemaForm />
+      <div>
+        <CinemaCard v-for="cinema in films" :key="cinema.name" :cinema="cinema" />
+      </div>
     </section>
   </PageLayout>
 </template>
@@ -11,14 +14,23 @@
 <script>
 import PageLayout from '../parts/PageLayout'
 import { helpModal } from "@/mixins/modals";
+import { mapActions } from "vuex";
 import CinemaForm from "../forms/CinemaForm"
+import CinemaCard from "../cinema/CinemaCard.vue"
 
 export default {
   name: 'HomePage',
   mixins: [helpModal],
   components: {
     PageLayout,
-    CinemaForm
+    CinemaForm,
+    CinemaCard
+  },
+  computed: {
+    ...mapActions([
+      'getFilms'
+    ]),
+    films: () => this.getFilms()
   }
 }
 </script>
