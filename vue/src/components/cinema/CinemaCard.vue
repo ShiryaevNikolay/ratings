902c63967406e671c097dca764347c8265e66192
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-10-12 17:38:39 +0300

    #6 - правки стилей

--- vue/src/components/cinema/CinemaCard.vue
+++ vue/src/components/cinema/CinemaCard.vue
@@ -1,14 +1,18 @@
 <template>
   <div v-if="cinema" class="cinema-card">
-    <img class="cinema-card__image" :src="cinema.previewUrl" />
-    <ul class="cinema-card__content">
-      <li class="cinema-card__name">{{ cinema.name }}</li>
-      <li class="cinema-card__origin-name">{{ cinema.originName }}</li>
-      <li class="cinema-card__producer">{{ cinema.producer }}</li>
-      <li class="cinema-card__date">{{ cinema.date }}</li>
-      <li class="cinema-card__score">
-        <el-rate v-model="getScore" disabled :colors="getSroceIcons" />  
+    <ul class="cinema-card__list">
+      <li>
+        <img class="cinema-card__image" :src="cinema.previewUrl" />
       </li>
+      <li class="cinema-card__score cinema-card__item">
+        <el-rate v-model="getScore" disabled :colors="getSroceIcons" />
+      </li>
+    </ul>
+    <ul class=" cinema-card__list cinema-card__info">
+      <li class="cinema-card__name">{{ cinema.name }}</li>
+      <li class="cinema-card__origin-name cinema-card__item">{{ cinema.originName }}</li>
+      <li class="cinema-card__producer cinema-card__item">{{ cinema.producer }}</li>
+      <li class="cinema-card__date cinema-card__item">{{ cinema.date }}</li>
     </ul>
   </div>
 </template>
@@ -22,10 +26,10 @@ export default {
     default: () => (null)
   },
   computed: {
-    getScore () {
+    getScore() {
       return this.cinema ? this.cinema.score : 0
     },
-    getSroceIcons () {
+    getSroceIcons() {
       return ['#99A9BF', '#F7BA2A', '#FF9900'] // same as { 2: 'icon-rate-face-1', 4: { value: 'icon-rate-face-2', excluded: true }, 5: 'icon-rate-face-3' }
     }
   }
@@ -40,16 +44,19 @@ export default {
     max-height: 200px;
   }
 
-  &__content {
+  &__list {
     display: inline-block;
     padding: 0px;
-    margin: 16px;
     list-style-type: none;
     vertical-align: top;
   }
 
-  &__name {
-    margin-bottom: 16px;
+  &__info {
+    margin: 16px;
+  }
+
+  &__item {
+    margin-top: 16px;
   }
 }
 </style>
