f2d2c29950848075c67b396604c109d48129c175
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-10-16 19:02:36 +0300

    #8 - форма создания фильма выносится в отдельную страницу

---
+++ vue/src/components/pages/CreateCinema.vue
@@ -0,0 +1,33 @@
+<template>
+  <PageLayout>
+    <section class="p-16">
+      <h1>Добавление рейтинга фильма</h1>
+      <CinemaForm :btnText="addCinemaBtnText" @btnClick="(cinema) => saveCinema(cinema)" />
+    </section>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from '../parts/PageLayout'
+import CinemaForm from "../forms/CinemaForm"
+import { helpCinema } from "@/mixins/cinema"
+
+export default {
+  name: "CreateCinema",
+  mixins: [helpCinema],
+  components: {
+    PageLayout,
+    CinemaForm
+  },
+  computed: {
+    addCinemaBtnText () {
+      return "Добавить фильм"
+    }
+  },
+  methods: {
+    saveCinema (cinema) {
+      this.addCinema(cinema)
+    }
+  }
+}
+</script>
