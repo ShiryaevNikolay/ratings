f77e82e8c63e453e73991301460a17e6134feca4
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-10-18 15:59:28 +0300

    #8 - синхронизация с localStorage

--- vue/src/store/cinema.js
+++ vue/src/store/cinema.js
@@ -14,29 +14,44 @@ export const genHash = (length = 32) => {
   return hash + Date.now();
 }
 
+const storeKey = 'CINEMA_STORE'
+
+const syncStateWithLocalStorage = (state) => {
+  localStorage.setItem(storeKey, JSON.stringify(state.films))
+}
+const getStateFromLocalStorage = () => {
+  const films = localStorage.getItem(storeKey)
+  if (films) {
+    return JSON.parse(films)
+  } else {
+    return []
+  }
+}
+
 export default {
   namespaced: true,
   state: {
-    films: [
-      {
-        id: 1,
-        previewUrl: 'https://images.unsplash.com/photo-1693711942336-f4f9963bd364?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80',
-        name: 'Фильм 1',
-        originName: 'Оригинальне название',
-        date: '2020-07-03',
-        producer: 'Имя режиссера',
-        score: 4
-      },
-      {
-        id: 2,
-        previewUrl: 'https://images.unsplash.com/photo-1697213799599-de2d228124e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80',
-        name: 'Фильм 2',
-        originName: 'Film 2',
-        date: '2020-10-05',
-        producer: 'Режиссер',
-        score: 2
-      }
-    ]
+    films: getStateFromLocalStorage()
+    // films: [
+    //   {
+    //     id: 1,
+    //     previewUrl: 'https://images.unsplash.com/photo-1693711942336-f4f9963bd364?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80',
+    //     name: 'Фильм 1',
+    //     originName: 'Оригинальне название',
+    //     date: '2020-07-03',
+    //     producer: 'Имя режиссера',
+    //     score: 4
+    //   },
+    //   {
+    //     id: 2,
+    //     previewUrl: 'https://images.unsplash.com/photo-1697213799599-de2d228124e6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1920&q=80',
+    //     name: 'Фильм 2',
+    //     originName: 'Film 2',
+    //     date: '2020-10-05',
+    //     producer: 'Режиссер',
+    //     score: 2
+    //   }
+    // ]
   },
   getters: {
     getFilms: (state) => state.films,
@@ -46,12 +61,15 @@ export default {
     addCinema (state, payload) {
       payload.id = genHash()
       state.films.push(payload)
+      syncStateWithLocalStorage(state)
     },
     removeCinema (state, payload) {
       state.films = state.films.filter((cinema) => cinema.id != payload)
+      syncStateWithLocalStorage(state)
     },
     editCinema (state, payload) {
       state.films = state.films.map((cinema) => cinema.id == payload.id ? payload : cinema)
+      syncStateWithLocalStorage(state)
     }
   }
 }
