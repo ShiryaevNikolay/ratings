745619c7dee9618cd6286e535333eeb75ab48cca
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-10-26 17:23:00 +0300

    #10 - стилизация страницы

---
+++ vue/src/components/cinema/CinemaPreview.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="cinema-preview">
+    <img :src="cinema.previewUrl" class="cinema-preview__image"/>
+    <div class="cinema-preview__name">{{ cinema.name }}</div>
+    <div>{{ cinema.date | dateFormat }}</div>
+    <div class="cinema-preview__author">{{ cinema.producer }}</div>
+    <div>{{ cinema.score }}</div>
+  </div>
+</template>
+
+<script>
+import { helpCommon } from "@/mixins/common"
+
+export default {
+  name: "CinemaPreview",
+  mixins: [helpCommon],
+  props: {
+    cinema: {
+      type: Object,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.cinema-preview {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  &__name {
+    margin-top: 20px;
+  }
+
+  &__author {
+    margin-bottom: 20px;
+  }
+
+  &__name {
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  &__image {
+    height: 350px;
+    border-radius: 4px;
+  }
+}
+</style>
--- vue/src/components/pages/RatingFilms.vue
+++ vue/src/components/pages/RatingFilms.vue
@@ -1,18 +1,86 @@
 <template>
   <PageLayout>
-    <div>
-      Рейтинг фильмов
-    </div>
+    <section class="p-16">
+      <h1>Составление рейтинга</h1>
+      <div class="rating">
+        <div class="rating__cinema">
+          <CinemaPreview :cinema="films[0]" />
+          <div class="rating__controls">
+            <ElButton type="success">{{ "+10" }}</ElButton>
+            <div class="rating__controls__container">
+              <ElButton type="primary">{{ "+1" }}</ElButton>
+              <span>Lorem</span>
+            </div>
+          </div>
+        </div>
+        <div class="rating__divider"/>
+        <div class="rating__cinema">
+          <CinemaPreview :cinema="films[1]" />
+          <div class="rating__controls">
+            <div class="rating__controls__container">
+              <span>Lorem</span>
+              <ElButton type="primary">{{ "+1" }}</ElButton>
+            </div>
+            <ElButton type="success">{{ "+10" }}</ElButton>
+          </div>
+        </div>
+      </div>
+    </section>
   </PageLayout>
 </template>
 
 <script>
 import PageLayout from '../parts/PageLayout'
+import CinemaPreview from '../cinema/CinemaPreview.vue';
+import { mapGetters } from 'vuex';
 
 export default {
   name: "RatingFilms",
   components: {
-    PageLayout
+    PageLayout,
+    CinemaPreview
+  },
+  computed: {
+    ...mapGetters('cinema', [
+      'getFilms'
+    ]),
+    films() {
+      return this.getFilms
+    }
   }
 }
 </script>
+
+<style scoped lang="less">
+.rating {
+  display: grid;
+  grid-template-columns: 1fr 1px 1fr;
+  gap: 20px;
+  margin: 16px;
+  margin-top: 40px;
+
+  &__divider {
+    background-color: gray;
+  }
+
+  &__cinema {
+    display: flex;
+    flex-direction: column;
+    widows: 100%;
+  }
+
+  &__controls {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 20px;
+
+    &__container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 16px;
+    }
+  }
+}
+</style>
