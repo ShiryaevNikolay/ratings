1d97c5bd121ff8ec129d55acb20e1fb1c596d74f
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-11-16 21:03:29 +0300

    #14 - страница настроек

---
+++ vue/src/components/pages/SettingsPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <PageLayout>
+    <section class="p-16">
+      <h1>Настройки</h1>
+    </section>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from '../parts/PageLayout'
+
+export default {
+  name: 'SettingsPage',
+  components: {
+    PageLayout
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
